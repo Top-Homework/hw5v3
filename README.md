645725b7d9c9e58556efcc1de5613f0d09af4492
lcarranco <lcarranco@gmail.com>
2017-04-20 20:30:19 -0500

    Added ignoreline function
    Added getsize function
    
    Changed container from list to vector
    
    Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # hw5v3
+
+* ifstream::pos_type - streampos
+* tellg() - Get position in input sequence
+* clear() - Set error state flags. Sets a new value for the stream's internal error state flags. The current value of the flags is overwritten
+* seekg() - Set position in input sequence
+* tellg() - Get position in input sequence
+* ignore(streamsize n, delim) - Extract and discard characters
+* numberic_limits<streamsize>::max() - Maximum finite value
--- reachability.cpp
+++ reachability.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
-#include <list>
+#include <fstream>
+#include <limits>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class Graph {
     int V;
-    list<int> *adj;
+    vector<int> *adj;
 
   public:
     Graph(int V) {
         this->V = V;
-        adj = new list<int>[ V ];
+        adj = new vector<int>[ V ];
     }
 
-    void addEdge(int v, int w) {
+    void addEdge(unsigned int v, int w) {
         adj[v].push_back(w);
     }
 
@@ -22,17 +25,17 @@ class Graph {
         for (int i = 0; i < V; i++) {
             visited[i] = false;
         }
-        list<int> queue;
+        vector<int> queue;
 
         visited[s] = true;
         queue.push_back(s);
 
-        list<int>::iterator i;
+        vector<int>::iterator i = queue.begin();
 
         while (!queue.empty()) {
             s = queue.front();
             cout << s << " ";
-            queue.pop_front();
+            queue.erase(queue.begin());
             for (i = adj[s].begin(); i != adj[s].end(); ++i) {
                 if (!visited[*i]) {
                     visited[*i] = true;
@@ -44,9 +47,48 @@ class Graph {
 
 };
 
+istream& ignoreline(ifstream& in, ifstream::pos_type& pos)
+{
+	pos = in.tellg();
+	return in.ignore(numeric_limits<streamsize>::max(), '\n');
+}
 
-int main() {
-    Graph g(4);
+int getsize(ifstream& in) {
+    ifstream::pos_type pos = in.tellg();
+
+    ifstream::pos_type lastPos;
+    while (in >> ws && ignoreline(in, lastPos)) {
+        pos = lastPos;
+    }
+    in.clear();
+    in.seekg(pos);
+
+    string line;
+    getline(in, line, ' ');
+	int size = stoi(line);
+    return size;
+}
+
+int main(int argc, char *argv[]) {
+    // if(argc < 2) {
+    //     //cerr("Usage: reachability \"E=input.txt;source=num\"\n");
+    //     return 1;
+    // }
+    // ArgumentManager am(argc, argv);
+    // string filename = am.get("E");
+    // int source = stoi(am.get("source"));
+    // ifstream ifs(filename, ios::in);
+
+    // Used for debugging on Visual Studio
+    ifstream ifs("1.txt", ios::in);
+    int source = 2;
+
+	int size = 0;
+	if (ifs) {
+		size = getsize(ifs);
+	}
+
+    Graph g(size);
     g.addEdge(0, 1);
     g.addEdge(0, 2);
     g.addEdge(1, 2);
@@ -58,6 +100,7 @@ int main() {
          << "(starting from vertex 2) \n";
     g.BFS(2);
     cout << endl;
+	getchar();
 
     return 0;
 }
