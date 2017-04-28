f1fe63776b986fe9fa6273f4a38a3175ede07119
lcarranco <lcarranco@gmail.com>
2017-04-27 22:28:58 -0500

    Overrode my changes with original code from Geeks for Geeks

--- reachability.cpp
+++ reachability.cpp
@@ -9,60 +9,76 @@
 
 using namespace std;
 
-class Graph {
-    int V; // Number of vertices
-    list<int> *adj; // Pointer to an array containing adjacency lists
-
-  public:
-    Graph(int V) {
-        this->V = V;
-        adj = new list<int>[ V ];
-    }
-
-	~Graph() {
-		delete[] adj;
-		adj = nullptr;
-	}
-
-    void addEdge(int v, int w) {
-        adj[v].push_back(w); // Add w to v's list
-    }
-
-    void BFS(int s) {
-		// Marks all vertices as not visited
-        bool *visited = new bool[V];
-        for (int i = 0; i < V; i++) {
-            visited[i] = false;
-        }
-
-		// Creates a queue for BFS
-        list<int> queue;
-
-		// Martk the current node as visited and push it to list
-        visited[s] = true;
-        queue.push_back(s);
-
-		// 'i' will be used to get all adjacent vertices of a vertex
-		list<int>::iterator i;
-
-        while (!queue.empty()) {
-			// Print vertex and pop from front of list
-            s = queue.front();
-            cout << s << " ";
-			queue.pop_front();
-
-			// Get all adjacent vertices of the popped vertex s
-			// If an adjacent has not been visited then mark it as visited and push it to the list
-            for (i = adj[s].begin(); i != adj[s].end(); ++i) {
-                if (!visited[*i]) {
-                    visited[*i] = true;
-                    queue.push_back(*i);
-                }
-            }
-        }
-    }
-
+// This class represents a directed graph using adjacency list representation
+class Graph
+{
+	int V; // No. of vertices
+	list<int> *adj; // Pointer to an array containing adjacency lists
+	int source;
+public:
+	Graph(int V, int source); // Constructor
+	~Graph();
+	void addEdge(int v, int w); // function to add an edge to graph
+	void BFS(int s); // prints BFS traversal from a given source s
 };
+
+Graph::Graph(int V, int source)
+{
+	this->V = V;
+	adj = new list<int>[V + 1];
+	this->source = source;
+}
+
+Graph::~Graph() {
+	adj = nullptr;
+	delete[] adj;
+}
+
+void Graph::addEdge(int v, int w)
+{
+	adj[v].push_back(w); // Add w to v's list.
+}
+
+void Graph::BFS(int s)
+{
+	// Mark all the vertices as not visited
+	bool *visited = new bool[V + 1];
+	for (int i = 0; i <= V; i++)
+		visited[i] = false;
+
+	// Create a queue for BFS
+	list<int> queue;
+
+	// Mark the current node as visited and enqueue it
+	visited[s] = true;
+	queue.push_back(s);
+
+	// 'i' will be used to get all adjacent vertices of a vertex
+	list<int>::iterator i;
+
+	while (!queue.empty())
+	{
+		// Dequeue a vertex from queue and print it
+		s = queue.front();
+		if (s != source) {
+			cout << s << " ";
+		}
+		queue.pop_front();
+
+		// Get all adjacent vertices of the dequeued vertex s
+		// If a adjacent has not been visited, then mark it visited
+		// and enqueue it
+		for (i = adj[s].begin(); i != adj[s].end(); ++i)
+		{
+			if (!visited[*i])
+			{
+				visited[*i] = true;
+				queue.push_back(*i);
+			}
+		}
+	}
+	delete[] visited;
+}
 
 int main(int argc, char *argv[]) {
     // if(argc < 2) {
@@ -78,10 +94,10 @@ int main(int argc, char *argv[]) {
     // temp.open("temp.txt");
 
     // Used for debugging on Visual Studio
-    string filename = ("5.txt");
+    string filename = ("4.txt");
     ifstream ifs;
     ifs.open(filename);
-    int source = 2;
+    int source = 1;
 
     // The result of the read is placed in here
     // In C++, we use a vector like an array but vectors can dynamically grow
@@ -107,19 +123,17 @@ int main(int argc, char *argv[]) {
 	size = data.back().at(0); // Retrieves size from last elemnt
 	data.pop_back(); //Deletes last line of vector
 
-    Graph g(size);
+    Graph g(size, source);
 
-	for (int i = 0; i < data.size() - 1; i++) {
+	for (int i = 0; i < data.size(); i++) {
 		g.addEdge(data[i][0], data[i][1]);
-		cout << "\t" << data[i][0] << " " << data[i][1] << endl;
+		// cout << "\t" << data[i][0] << " " << data[i][1] << endl;
 	}
-	cout << endl;
+	// cout << endl;
 
-    cout << "\tFollowing is Breadth First Traversal "
-            << "(starting from vertex n) \n";
+    cout << "\tFollowing is Breadth First Traversal (starting from vertex n) \n";
     g.BFS(source);
-    cout << endl;
-	getchar();
-
+    // cout << endl;
+	// getchar();
     return 0;
 }
