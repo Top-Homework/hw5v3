b43133ff485e23f4216afaffbf6065137201e61e
lcarranco <lcarranco@gmail.com>
2017-04-27 23:20:08 -0500

    Cleaned up code

--- reachability.cpp
+++ reachability.cpp
@@ -16,75 +16,65 @@ class Graph
 	int V; // No. of vertices
 	list<int> *adj; // Pointer to an array containing adjacency lists
 	int source;
-public:
-	Graph(int V, int source); // Constructor
-	~Graph();
-	void addEdge(int v, int w); // function to add an edge to graph
-	void BFS(int s); // prints BFS traversal from a given source s
-};
 
-Graph::Graph(int V, int source)
-{
-	this->V = V;
-	adj = new list<int>[V + 1];
-	this->source = source;
-}
+  public:
+	// Constructor
+	Graph(int V, int source) {
+		this->V = V;
+		adj = new list<int>[ V + 1 ];
+		this->source = source;
+	}
 
-Graph::~Graph() {
-	adj = nullptr;
-	delete[] adj;
-}
+	// function to add an edge to graph
+	void addEdge(int v, int w) {
+		adj[v].push_back(w); // Add w to v's list.
+	}
 
-void Graph::addEdge(int v, int w)
-{
-	adj[v].push_back(w); // Add w to v's list.
-}
+	// prints BFS traversal from a given source s
+	void BFS(int s) {
+		// Mark all the vertices as not visited
+		bool *visited = new bool[V + 1];
+		for (int i = 0; i <= V; i++)
+			visited[i] = false;
 
-void Graph::BFS(int s)
-{
-	// Mark all the vertices as not visited
-	bool *visited = new bool[V + 1];
-	for (int i = 0; i <= V; i++)
-		visited[i] = false;
+		// Create a queue for BFS
+		list<int> queue;
 
-	// Create a queue for BFS
-	list<int> queue;
+		// Mark the current node as visited and enqueue it
+		visited[s] = true;
+		queue.push_back(s);
 
-	// Mark the current node as visited and enqueue it
-	visited[s] = true;
-	queue.push_back(s);
+		// 'i' will be used to get all adjacent vertices of a vertex
+		list<int>::iterator i;
 
-	// 'i' will be used to get all adjacent vertices of a vertex
-	list<int>::iterator i;
+		list<int> vertices;
+		while (!queue.empty()) {
+			// Dequeue a vertex from queue and print it
+			s = queue.front();
+			if (s != source) {
+				vertices.push_back(s);
+				//cout << s << " ";
+			}
+			queue.pop_front();
 
-	list<int> vertices;
-	while (!queue.empty())
-	{
-		// Dequeue a vertex from queue and print it
-		s = queue.front();
-		if (s != source) {
-			vertices.push_back(s);
-			//cout << s << " ";
-		}
-		queue.pop_front();
-
-		// Get all adjacent vertices of the dequeued vertex s
-		// If a adjacent has not been visited, then mark it visited
-		// and enqueue it
-		for (i = adj[s].begin(); i != adj[s].end(); ++i)
-		{
-			if (!visited[*i])
-			{
-				visited[*i] = true;
-				queue.push_back(*i);
+			// Get all adjacent vertices of the dequeued vertex s
+			// If a adjacent has not been visited, then mark it visited
+			// and enqueue it
+			for (i = adj[s].begin(); i != adj[s].end(); ++i) {
+				if (!visited[*i]) {
+					visited[*i] = true;
+					queue.push_back(*i);
+				}
 			}
 		}
+
+		vertices.sort();
+		for (list<int>::iterator it = vertices.begin(); it != vertices.end(); it++) {
+			cout << *it << " ";
+		}
 	}
-	vertices.sort();
-	for (list<int>::iterator it = vertices.begin(); it != vertices.end(); it++) {
-		cout << *it << " ";
-	}
-}
+	
+};
 
 int main(int argc, char *argv[]) {
     if(argc < 2) {
